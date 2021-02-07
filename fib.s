00442717b350d18752d4ef85ae284a5c3032f85b
Zeyu-Li <zeyu.li23@gmail.com>
2021-02-06 23:08:25 -0600

    handles trivial cases

--- fib.s
+++ fib.s
@@ -1,6 +1,7 @@
 .data
     prompt1: .asciiz "Enter the iternation count: "
     prompt2: .asciiz "The Fibonacci number is: "
+    newLine: .ascii  "\n"
 
 .text
 main:
@@ -13,11 +14,45 @@ main:
     li  $v0, 5          # v0 = 4 is read in string
     syscall
 
-    # store in t0
+    # store in a0
     move $a0, $v0       # input is now in a0
 
-    # debug print
-    li $v0 1
+    # handles trivial case 1
+    blez  $a0, trivialCase1  # jumps to trivial case if a0 is 0 or less
+    # handles trivial case 2
+    li  $t0, 1
+    beq  $a0, $t0, trivialCase2  # jumps to trivial case if a0 is 1
+    fib:
+        addi $a0, -1    # decrement a0
+    
+    end:
+
+    # print prompt2
+    la  $a0, prompt2    # load string prompt1 into a0
+    li  $v0, 4          # v0 = 4 is print string
+    syscall
+
+    # print answer
+    move $a0, $s0       # s0 stores answer to move to a0 stores the answer
+    li $v0 1            # print int
+    syscall
+
+    # print new line
+    la  $a0, newLine    # load string prompt1 into a0
+    li  $v0, 4          # v0 = 4 is print string
     syscall
 
 	jr	$ra             # end program
+
+trivialCase1:
+    li $s0, 0           # stores trival answer as 0
+    j end               # jumps to program end
+
+trivialCase2:
+    li $s0, 1           # stores trival answer as 0
+    j end               # jumps to program end
+
+debug:
+    # debug print
+    li $v0 1
+    syscall
